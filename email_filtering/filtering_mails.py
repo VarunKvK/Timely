b6e4c3f4d80ac57db6fb0374d60be6410b453dce
VarunKvK <varunkrishnankv2002@gmail.com>
2024-03-13 18:18:00 +0530

    need to do some adjustments in this and this snippet contains csv library

--- email_filtering/filtering_mails.py
+++ email_filtering/filtering_mails.py
@@ -1,9 +1,18 @@
 import imaplib as im
 import email
 import os 
+import csv
 from dotenv import load_dotenv
 
 load_dotenv()
+
+email_list = []
+def read_email_list_to_monitor(email_csv):
+    with open(email_csv, "r") as email:
+        reader=csv.reader(email)
+        for row in reader:
+            email_list.append(row)
+    return(email_list)
 
 def retrieve_emails():
     try:
@@ -12,26 +21,25 @@ def retrieve_emails():
         
         mail.select('inbox')
         
-        result,data= mail.search(None,"FROM",'newsletter@rosa.do')
-        if result=="OK":
-            print(result)
-            for num in data[0].split():
-                result, email_data= mail.fetch(num,'(RFC822)')
-                
-                raw_email=email_data[0][1] 
-                msg = email.message_from_bytes(raw_email)  
-                sender=msg["From"]
-                subject=msg["Subject"]
-                body=""
-            if msg.is_multipart:
-                for msg_part in msg.walk():
-                    content_type=msg_part.get_content_type()
-                    if content_type=="text/plain" or content_type=="text/html":
-                        body+=msg_part.get_payload(decode=True).decode("utf-8")
-                        print(body)
-                    else:
-                        body=msg_part.get_payload(decode=True).decode()
-                        print(body)
+        for mails in email_list:
+            result,data= mail.search(None,"FROM",mails)
+            if result=="OK":
+                for num in data[0].split():
+                    result, email_data= mail.fetch(num,'(RFC822)')
+                    
+                    raw_email=email_data[0][1] 
+                    msg = email.message_from_bytes(raw_email)  
+                    sender=msg["From"]
+                    if sender==mails:                    
+                        if msg.is_multipart:
+                            for msg_part in msg.walk():
+                                content_type=msg_part.get_content_type()
+                                if content_type=="text/plain" or content_type=="text/html":
+                                    body+=msg_part.get_payload(decode=True).decode("utf-8")
+                                    print(body)
+                                else:
+                                    body=msg_part.get_payload(decode=True).decode()
+                                    print(body)
                      
     except Exception as e:
         print("An error occurred:", e)
@@ -40,4 +48,3 @@ def retrieve_emails():
         # Close the connection
         mail.close()
         mail.logout()
-    
