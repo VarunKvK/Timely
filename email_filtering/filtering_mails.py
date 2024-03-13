340ae238a7c0b3dbf9f897c1b0ef2e30bc98142c
VarunKvK <varunkrishnankv2002@gmail.com>
2024-03-13 18:00:07 +0530

    Pnds using for email scheduling

--- email_filtering/filtering_mails.py
+++ email_filtering/filtering_mails.py
@@ -1,37 +1,37 @@
 import imaplib as im
 import email
 import os 
+import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def retrieve_emails():
+def read_email_list_to_monitor(email_csv):
+    email=pd.read_csv(email_csv)
+    email_list=email.Email.tolist()
+    return email_list
+
+def retrieve_emails(emails):
     try:
         mail=im.IMAP4_SSL('imap.gmail.com')
         mail.login(os.getenv('EMAIL_SECONDARY'),os.getenv('PASSWORD_SECONDARY'))
         
         mail.select('inbox')
-        
-        result,data= mail.search(None,"FROM",'newsletter@rosa.do')
-        if result=="OK":
-            print(result)
-            for num in data[0].split():
-                result, email_data= mail.fetch(num,'(RFC822)')
-                
-                raw_email=email_data[0][1] 
-                msg = email.message_from_bytes(raw_email)  
-                sender=msg["From"]
-                subject=msg["Subject"]
-                body=""
-            if msg.is_multipart:
-                for msg_part in msg.walk():
-                    content_type=msg_part.get_content_type()
-                    if content_type=="text/plain" or content_type=="text/html":
-                        body+=msg_part.get_payload(decode=True).decode("utf-8")
-                        print(body)
-                    else:
-                        body=msg_part.get_payload(decode=True).decode()
-                        print(body)
+        # print(emails)
+        for mails in emails:
+            try:
+                print(mails)
+                result,data=mail.search(None,"FROM",mails)
+                if result=="OK":
+                    for num in data[0].split():
+                        result, email_data= mail.fetch(num,'(RFC822')
+                        raw_msg=email_data[0][1]
+                        msg=email.message_from_bytes(raw_msg)
+                        sender=msg["From"]
+                        print(sender)
+            except Exception as e:
+                print(f"The particular {mails} doesn't exists so skipping it and continue .....")
+                continue                                            
                      
     except Exception as e:
         print("An error occurred:", e)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from email_filtering import filtering_mails
 def main():
-    filtering_mails.retrieve_emails()
+    csvfile="./emails/email_monitoring.csv"
+    # sender_email=input("Enter the email address to monitor: ")
+    emails=filtering_mails.read_email_list_to_monitor(email_csv=csvfile)
+    filtering_mails.retrieve_emails(emails)
     
 if __name__ == "__main__":
     main()
